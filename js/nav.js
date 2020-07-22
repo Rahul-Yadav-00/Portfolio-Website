817100d1dd324a0c921360dcd6151135da7ec3db
Rahul-Yadav-00 <rahulyadav5524@gmail.com>
2020-07-22 15:50:43 +0530

    new college website project is added

--- js/nav.js
+++ js/nav.js
@@ -5,9 +5,5 @@ const navSlide= ()=>{
     burger.addEventListener('click',()=>{
         nav.classList.toggle('nav-active');
     });
-    // navLinks.forEach((link,index)=>{
-    //     link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7+0.3}s';
-    //     console.log(index/7);
-    // });
 }
 navSlide();
